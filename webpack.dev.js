f047ad806930a3dc9d8f29b314be87d39cdb093d
Junhao Liao <junhao@junhao.ca>
2024-04-18 21:19:14 -0400

    Add `react-refresh` to allow hot reload without reloading full page; Upgrade babel dependencies; clean up webpack configs.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,46 @@
-const {merge} = require("webpack-merge");
+const {merge, mergeWithRules} = require("webpack-merge");
 const common = require("./webpack.common.js");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const path = require("path");
 
-module.exports = merge(common, {
+
+module.exports = mergeWithRules({
+    module: {
+        rules: {
+            test: "match",
+            use: "replace",
+        },
+    },
+})(common, {
     mode: "development",
     devtool: "cheap-module-source-map",
     devServer: {
+        hot: true,
         port: 3010,
         static: "./dist",
     },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: path.resolve(__dirname, "src"),
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                        ],
+                        plugins: [
+                            require.resolve("react-refresh/babel"),
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new ReactRefreshWebpackPlugin(),
+    ],
 });
