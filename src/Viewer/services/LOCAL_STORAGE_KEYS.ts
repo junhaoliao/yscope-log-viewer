19a5625186b22e762f0504159239baf97fdc6272
Junhao Liao <junhao@junhao.ca>
2024-04-30 15:48:47 -0400

    Converted LOCAL_STORAGE_KEYS.js to TypeScript.

---
+++ src/Viewer/services/LOCAL_STORAGE_KEYS.ts
@@ -0,0 +1,9 @@
+/**
+ * Enum of the keys used to store data in local storage.
+ */
+enum LOCAL_STORAGE_KEYS {
+    UI_THEME = "uiTheme",
+    PAGE_SIZE = "pageSize",
+}
+
+export default LOCAL_STORAGE_KEYS;
