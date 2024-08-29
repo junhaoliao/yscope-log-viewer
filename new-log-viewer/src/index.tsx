44082e7d3d61bef7d3017c5748ceb1887c2c086c
Junhao Liao <junhao@junhao.ca>
2024-08-29 18:12:54 -0400

    gh-action-test: violation 2.

--- new-log-viewer/src/index.tsx
+++ new-log-viewer/src/index.tsx
@@ -6,6 +6,8 @@ import App from "./App";
 import "./index.css";
 
 
+// TODO: violation 2
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(document.getElementById("root")!);
 root.render(
