93768d191a264e41ed3ca4546651b683613199d6
junhao-ca <ljhhappyfish@gmail.com>
2024-09-16 12:57:06 -0400

    Update App.tsx

--- new-log-viewer/src/App.tsx
+++ new-log-viewer/src/App.tsx
@@ -4,6 +4,7 @@ import UrlContextProvider from "./contexts/UrlContextProvider";
 
 
 // FIXME: this is a lint violation.
+// FIXME: this is another lint violation.
 
 /**
  * Renders the main application.
